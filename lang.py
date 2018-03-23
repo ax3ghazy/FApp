8b8cab17f3d34dd7b84ff5112aeae3002131cb69
abdelshahied <37671918+abdelshahied@users.noreply.github.com>
2018-03-23 09:39:34 +0000

    Update lang.py

--- lang.py
+++ lang.py
@@ -1,23 +1,29 @@
 import boto3
 
-client = boto3.client('s3')
+def ded_senti (buck, fil, path):
+	client = boto3.client('s3')
 
-response = client.create_bucket(Bucket='abdelshahied')
-client.upload_file('test.txt', 'abdelshahied', 'test.txt')
+	response = client.create_bucket(Bucket=buck)
+	client.upload_file(fil, buck, fil)
 
 
-path = "/home/ubuntu/Desktop/test.txt"
-com_file = open(path, 'r')
+	#path = "/home/ubuntu/Desktop/test.txt"
+	com_file = open(path, 'r')
 
-comp = boto3.client('comprehend')
+	comp = boto3.client('comprehend')
 
-response = comp.detect_dominant_language(
-    Text = com_file.readline(),
-)
-x = 0.0
-for language in response['Languages']:
-	if language ['Score'] > x:
-		code = language ['LanguageCode']
-		x = language ['Score']
+	response = comp.detect_dominant_language(
+    	Text = com_file.readline(),
+	)
+	x = 0.0
+	for language in response['Languages']:
+		if language ['Score'] > x:
+			code = language ['LanguageCode']
+			x = language ['Score']
 	
-print (code)	
+	print (code)	
+	return code
+
+
+#ded_senti ('abdelshahied', 'test.txt', "/home/ubuntu/Desktop/test.txt")
+
